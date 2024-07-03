9bc2aed8a420d58aafb9da1361084a0dc7f8ccad
ravindusr <ravindurashmika121@gmail.com>
2024-07-03 03:42:23 -0700

    js

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ document.addEventListener("DOMContentLoaded", () => {
         navLinks.classList.toggle('active');
     });
 });
+
+ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+});
+
+ScrollReveal().reveal('.home-content,heading', { origin:'top' });
+ScrollReveal().reveal('.home-img , .contact form , .experience-container', { origin:'bottom' });
+ScrollReveal().reveal('.home-contact h1 , .about-img ', { origin:'left' });
+ScrollReveal().reveal('.home-contact p , .about-content', { origin:'right' });
+
+
+const typed =new Typed('.multiple-text',{
+    strings:['Software Engineer ', 'Web Developer'],
+    typeSpeed:70,
+    backSpeed:70,
+    backDelay:1000,
+    loop: true,
+});
